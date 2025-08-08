9f7929423d952f1e74d332bfa9944225da62df49
ani67h <ghoshanindya4589@gmail.com>
2025-08-08 20:20:53 +0600

    sets and arrays

---
+++ arrays.py
@@ -0,0 +1,12 @@
+# create an array
+array_num = [1, 3, 5, 3, 7, 9, 3]
+print("Original arrays : ",array_num)
+
+print("First element : ",array_num[0])
+# count number of occurence
+print("Number of occurence of the number 7 in he said array : " + str(array_num.count(3)))
+
+# reverse the array
+array_num.reverse()
+print("Reverse the order of the items : ")
+print(array_num)
---
+++ setintersectionandunion.py
@@ -0,0 +1,12 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Original set elements : ")
+print(setx)
+print(sety)
+print("\nIntersectin of two said sets : ") 
+setz = setx.intersection(sety) # intersetion of two sets
+print(setz)
+
+print("\nUnion of two said sets : ")
+setz = setx.union(sety) # union of two sets
+print(setz)
