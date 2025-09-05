7c4cc267d5d03b6a9036b7332ec11fbf1437570b
ani67h <ghoshanindya4589@gmail.com>
2025-09-05 17:04:23 +0600

    listcomprehension

---
+++ listcomprehension.py
@@ -0,0 +1,14 @@
+# list comprehension
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+
+even = [ x for x in numbers if x%2 == 0 ]
+square_root = [y for y in numbers if y % y**(1/3) == 0]
+prime_num = [p for p in numbers if p > 1 and not [i for i in range(2, p) if p % i ==0]] # taken from youtube and chatgpt
+
+print("The even number from 1 to 100 are ",even)
+print("Among the numbers the perfect cube numbers are ",square_root)
+print("The prime number in the list are ",prime_num)
+
+# dictionary comprehension
+mydict = {str(z): z**2 for z in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+print("Squares of the numbers from 1 to 10 : ",mydict)
